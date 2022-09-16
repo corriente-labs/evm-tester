6576fe6c921305cedb6ccd3929eed1fc8e903562
0xys <yushi.kinger@gmail.com>
2022-09-16 17:52:43 +0900

    support bytecode. not tested yet

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
-use std::{
-    io::BufReader,
-    fs::File
-};
 use glob::glob;
 use reader::read_stateless;
-use string_builder::Builder;
 use std::io::prelude::*;
+use std::{fs::File, io::BufReader};
+use string_builder::Builder;
 
 mod core;
-mod reader;
 mod executor;
 mod mover;
+mod reader;
 
-use crate::core::{Input, Output, TestCase, FileType};
+use crate::core::{FileType, Input, Output, TestCase};
 use crate::executor::executor::execute;
 use crate::mover::mover::to_move_test;
 
@@ -59,7 +56,7 @@ fn read(filepath: &str) -> std::io::Result<Vec<Input>> {
 }
 
 #[allow(dead_code)]
-fn extract_testname(path: &str)-> String {
+fn extract_testname(path: &str) -> String {
     let filename = path.split("/").last().unwrap();
     let filename = filename.to_owned();
     let testname = filename.split(".").collect::<Vec<&str>>()[0];
@@ -67,7 +64,6 @@ fn extract_testname(path: &str)-> String {
 }
 
 fn main() {
-
     // read stateless huff
     for entry in glob("./resources/huff/*.huff").unwrap() {
         if let Ok(path) = entry {
@@ -76,7 +72,7 @@ fn main() {
             let testname = filename.to_owned().replace(".huff", "");
 
             let input = read_stateless(&src_path, FileType::Huff).unwrap();
-            
+
             let code = hex::decode(input.code).unwrap();
             let calldata = hex::decode(input.calldata).unwrap();
 
@@ -86,13 +82,46 @@ fn main() {
                 code,
                 value: input.value,
                 calldata,
-                output: res.clone()
+                output: res.clone(),
             };
 
-            let move_path = src_path.replace(".huff", "_test.move")
+            let move_path = src_path
+                .replace(".huff", "_test.move")
                 .replace("resources/huff", "artifacts/move");
             write_move_test(&testname, &move_path, &[testcase]).unwrap();
             println!("huff test case found. {:?} -> {:?}", src_path, move_path);
+        }
+    }
+
+    // read stateless bytecode
+    for entry in glob("./resources/bytecode/*.bytecode").unwrap() {
+        if let Ok(path) = entry {
+            let src_path = path.display().to_string();
+            let filename = path.file_name().unwrap().to_str().unwrap();
+            let testname = filename.to_owned().replace(".bytecode", "");
+
+            let input = read_stateless(&src_path, FileType::Bytecode).unwrap();
+
+            let code = hex::decode(input.code).unwrap();
+            let calldata = hex::decode(input.calldata).unwrap();
+
+            let res = execute(&code, &calldata, input.value);
+            let testcase = TestCase {
+                id: testname.to_owned(),
+                code,
+                value: input.value,
+                calldata,
+                output: res.clone(),
+            };
+
+            let move_path = src_path
+                .replace(".bytecode", "_test.move")
+                .replace("resources/bytecode", "artifacts/move");
+            write_move_test(&testname, &move_path, &[testcase]).unwrap();
+            println!(
+                "bytecode test case found. {:?} -> {:?}",
+                src_path, move_path
+            );
         }
     }
 
