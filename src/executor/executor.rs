a6c38f158dc6012141275a824050286a878c9253
0xys <yushi.kinger@gmail.com>
2022-09-16 17:22:31 +0900

    rustfmt

--- src/executor/executor.rs
+++ src/executor/executor.rs
@@ -4,23 +4,23 @@ use std::str::FromStr;
 use evm::backend::{MemoryAccount, MemoryBackend, MemoryVicinity};
 use evm::executor::stack::{MemoryStackState, StackExecutor, StackSubstateMetadata};
 use evm::Config;
-use primitive_types::{U256, H160};
+use primitive_types::{H160, U256};
 
 pub fn execute(code: &Vec<u8>, calldata: &Vec<u8>, balance: u128) -> Vec<u8> {
     let config = Config::london();
 
-	let vicinity = MemoryVicinity {
-		gas_price: U256::zero(),
-		origin: H160::default(),
-		block_hashes: Vec::new(),
-		block_number: Default::default(),
-		block_coinbase: Default::default(),
-		block_timestamp: Default::default(),
-		block_difficulty: Default::default(),
-		block_gas_limit: Default::default(),
-		chain_id: U256::one(),
-		block_base_fee_per_gas: U256::zero(),
-	};
+    let vicinity = MemoryVicinity {
+        gas_price: U256::zero(),
+        origin: H160::default(),
+        block_hashes: Vec::new(),
+        block_number: Default::default(),
+        block_coinbase: Default::default(),
+        block_timestamp: Default::default(),
+        block_difficulty: Default::default(),
+        block_gas_limit: Default::default(),
+        chain_id: U256::one(),
+        block_base_fee_per_gas: U256::zero(),
+    };
 
     let mut state = BTreeMap::new();
     state.insert(
@@ -30,7 +30,7 @@ pub fn execute(code: &Vec<u8>, calldata: &Vec<u8>, balance: u128) -> Vec<u8> {
             balance: U256::from(balance),
             storage: BTreeMap::new(),
             code: code.to_owned(),
-        }
+        },
     );
     state.insert(
         H160::from_str("0xf000000000000000000000000000000000000000").unwrap(),
