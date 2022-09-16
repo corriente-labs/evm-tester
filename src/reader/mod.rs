45015d59f9d105045dff1d03b1fecaccf8311aac
0xys <yushi.kinger@gmail.com>
2022-09-16 18:33:29 +0900

    test with bytecode success

--- src/reader/mod.rs
+++ src/reader/mod.rs
@@ -1,25 +1,35 @@
-use std::{
-    sync::Arc, fs,
-};
-use huff_core::Compiler;
 use anyhow::*;
+use huff_core::Compiler;
+use std::{fs, sync::Arc};
 
-use crate::core::{Input, FileType};
+use crate::core::{FileType, Input};
+
+fn strip_non_hex_chars(data: &str) -> String {
+    let original = data.to_owned();
+    original.replace("\n", "")
+}
 
 pub(crate) fn read_stateless(filepath: &str, filetype: FileType) -> anyhow::Result<Input> {
     let bytecode = match filetype {
         FileType::Huff => {
-            let compiler = Compiler::new(Arc::new(vec![filepath.to_owned()]), None, None, None, false, false);
+            let compiler = Compiler::new(
+                Arc::new(vec![filepath.to_owned()]),
+                None,
+                None,
+                None,
+                false,
+                false,
+            );
             let res = compiler.execute().unwrap();
             res[0].runtime.to_owned()
-        },
+        }
         FileType::Solidity => {
             bail!("Solidity not supported.")
-        },
+        }
         FileType::Bytecode => {
             let data = fs::read_to_string(filepath)?;
-            data
-        },
+            strip_non_hex_chars(&data)
+        }
     };
 
     let input = Input {
